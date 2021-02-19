95e0a64e6a78fba177ecfdec5d73904eb0ac54ac
VartikaKaul <78537604+VartikaKaul@users.noreply.github.com>
2021-02-19 14:09:06 +0530

    Update TransactionController.java
    Cloud Function changes

--- src/main/java/com/db/tax/controller/TransactionController.java
+++ src/main/java/com/db/tax/controller/TransactionController.java
@@ -3,16 +3,20 @@ package com.db.tax.controller;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @RestController()
-public class TransactionController {
+public class TransactionController implements HttpFunction {
 
     @GetMapping("/getTransactions")
     public String getTransactions() throws IOException {
+        System.out.println("Hello Smart Banking");
         return new String(Files.readAllBytes(Paths.get(new ClassPathResource("data.json").getFile().getAbsolutePath())));
     }
 }
