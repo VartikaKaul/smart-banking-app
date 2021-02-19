3e776b405030d5b35e7df0ced9a13fc695042b06
VartikaKaul <78537604+VartikaKaul@users.noreply.github.com>
2021-02-19 14:16:02 +0530

    Update BalanceController.java

--- src/main/java/com/db/tax/controller/BalanceController.java
+++ src/main/java/com/db/tax/controller/BalanceController.java
@@ -2,10 +2,18 @@ package com.db.tax.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
 
 @RestController()
-public class BalanceController {
+public class BalanceController implements HttpFunction {
 
+    @Override
+  public void service(HttpRequest request, HttpResponse response) throws Exception {
+    BufferedWriter writer = response.getWriter();
+    writer.write("50,0000 INR");
+  }
     @GetMapping("/checkBalance")
     public String checkBalance() {
         return "50000.00 INR";
