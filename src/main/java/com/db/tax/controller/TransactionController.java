f80a5cbee27e4a02fb47cdc2aa5766e4f1489238
VartikaKaul <78537604+VartikaKaul@users.noreply.github.com>
2021-02-19 14:23:35 +0530

    Update TransactionController.java

--- src/main/java/com/db/tax/controller/TransactionController.java
+++ src/main/java/com/db/tax/controller/TransactionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
-
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,6 +14,12 @@ import java.nio.file.Paths;
 @RestController()
 public class TransactionController implements HttpFunction {
 
+      @Override
+  public void service(HttpRequest request, HttpResponse response) throws Exception {
+    BufferedWriter writer = response.getWriter();
+    writer.write("Hello world!");
+  }
+    
     @GetMapping("/getTransactions")
     public String getTransactions() throws IOException {
         System.out.println("Hello Smart Banking");
