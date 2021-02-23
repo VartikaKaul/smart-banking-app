1b86818d1b51834eedcf5b3388b58cc077bf6dac
VartikaKaul <78537604+VartikaKaul@users.noreply.github.com>
2021-02-23 16:50:22 +0530

    patch-10

--- src/main/java/com/db/tax/controller/BalanceController.java
+++ src/main/java/com/db/tax/controller/BalanceController.java
@@ -8,21 +8,28 @@ import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @RestController()
 public class BalanceController implements HttpFunction {
 
     @Override
   public void service(HttpRequest request, HttpResponse response) throws Exception {
-        System.out.println(request.getBody());
-       /*  BufferedReader reader = request.getReader();
-        JSONTokener tokener = new JSONTokener(reader);
-        JSONObject json = new JSONObject(tokener);
-        System.out.println(json.get("request.body.queryResult.paramters.givenName"));
-        System.out.println(json.get("request.body.queryResult.paramters.unitCurrency.amount"));*/
-        BufferedWriter writer = response.getWriter();
+//         System.out.println(request.getBody());
+//        BufferedReader reader = request.getReader();
+//         JSONTokener tokener = new JSONTokener(reader);
+//         JSONObject json = new JSONObject(tokener);
+//         System.out.println(json.get("request.body.queryResult.paramters.givenName"));
+//         System.out.println(json.get("request.body.queryResult.paramters.unitCurrency.amount"));
+          String name;
+
+      JsonObject body = gson.fromJson(request.getReader(), JsonObject.class);
+	JSONObject qresult = body.getJSONObject("queryResult"); 
+      System.out.println("Body req"+ body.toString());
+      System.out.println("QueryResult" + qresult.toString());
+       BufferedWriter writer = response.getWriter();
         String responseString = "Request Received";
         writer.write(responseString);
   }
-    
 }
