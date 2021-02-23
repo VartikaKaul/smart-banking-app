7465e6172097bcabb91ce230ab0673ffc83b5627
Akash Chaudhuri <akashchaudhuri20@users.noreply.github.com>
2021-02-23 17:25:11 +0530

    patch-15

--- src/main/java/com/db/tax/controller/BalanceController.java
+++ src/main/java/com/db/tax/controller/BalanceController.java
@@ -3,8 +3,8 @@ package com.db.tax.controller;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+
 import java.io.BufferedWriter;
-import java.io.BufferedReader;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -16,10 +16,10 @@ public class BalanceController implements HttpFunction {
         private static final Gson gson = new Gson();
         String name;
         JsonObject body = gson.fromJson(request.getReader(), JsonObject.class);
-       // JSONObject qresult = body.getJSONObject("queryResult");
+        JsonObject qresult = body.getAsJsonObject("queryResult");
         System.out.println("Body: " + body.toString());
         System.out.println("Body req: " + body.toString());
-     //   System.out.println("QueryResult" + qresult.toString());
+        System.out.println("QueryResult" + qresult.toString());
         BufferedWriter writer = response.getWriter();
         String responseString = "Request Received";
         writer.write(responseString);
