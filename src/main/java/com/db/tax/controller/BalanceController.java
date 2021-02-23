8e41c4cec5e444ec53dec390e33013faba55ebbe
Akash Chaudhuri <akashchaudhuri20@users.noreply.github.com>
2021-02-23 17:11:35 +0530

    patch -14

--- src/main/java/com/db/tax/controller/BalanceController.java
+++ src/main/java/com/db/tax/controller/BalanceController.java
@@ -1,26 +1,25 @@
 package com.db.tax.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
-import org.json.JSONObject;
-import org.json.JSONTokener;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-@RestController()
 public class BalanceController implements HttpFunction {
 
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
+        private static final Gson gson = new Gson();
         String name;
         JsonObject body = gson.fromJson(request.getReader(), JsonObject.class);
-        JSONObject qresult = body.getJSONObject("queryResult");
-        System.out.println("Body req" + body.toString());
-        System.out.println("QueryResult" + qresult.toString());
+       // JSONObject qresult = body.getJSONObject("queryResult");
+        System.out.println("Body: " + body.toString());
+        System.out.println("Body req: " + body.toString());
+     //   System.out.println("QueryResult" + qresult.toString());
         BufferedWriter writer = response.getWriter();
         String responseString = "Request Received";
         writer.write(responseString);
